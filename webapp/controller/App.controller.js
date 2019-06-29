3af6d9f5c3a43abf517b19d720fb4b48c74ae0a7
Akmal Kilichbekov <akmal.kilichbekov@gmail.com>
2019-06-29 03:07:03 +0000

    third lecture - data binding

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,38 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-], function (Controller, MessageToast) {
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
 	"use strict";
 	return Controller.extend("opensap.myapp.controller.App", {
+		
+		onInit : function () {
+			// set data model on view
+			var JSONData = {
+				word:  "Hello World",
+				name: "John Doe",
+				qorbobo: "Qorbobogim oppoq"
+				
+			};
+			var oModel = new JSONModel(JSONData);
+			this.getView().setModel(oModel, "jsonGet");
+		},
+		
 		onPress: function () {
 			MessageToast.show("This is message");
+		},
+		
+		onClick: function(){
+			var oData = this.getView().getModel("jsonGet").getData();
+			var value = this.byId("inputID").getValue();
+			if(oData[value]){
+				MessageToast.show("Word exists in model");
+			}else{
+				MessageToast.show("Word does not exist");
+			}
+			
 		}
 	});
 });
+
+ 
